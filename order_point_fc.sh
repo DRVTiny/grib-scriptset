880de05e5ae267acc44ea22725c6bbf82f6fa1e4
Andrey A. Konovalov <drvtiny@gmail.com>
2013-04-23 14:28:46 +0400

    order_point_forecast now accept -f option and works with INI-files

--- order_point_fc.sh
+++ order_point_fc.sh
@@ -34,13 +34,16 @@ EOF
  return 0
 }
 
+source /opt/scripts/functions/config.func
+
 unset flTestOutCmds flResetSourcePath flParallelExec
 
 TEMP_DIR='/tmp'
 DATA_ID='Rostov-On-Don'
 nPredictHours=120
+pthStationsINI="$HOME/conf/areas.ini"
 
-while getopts 'yshxTi: P: m: e: d:' k; do
+while getopts 'yshxCTi: P: m: e: d: f:' k; do
  case $k in
   h) doShowUsage; exit 0      ;;
   x) set -x                   ;;
@@ -53,6 +56,8 @@ while getopts 'yshxTi: P: m: e: d:' k; do
   m) mode="${OPTARG,,}"       ;;
   e) emailTO="$OPTARG"	      ;;
   P) nPredictHours="$OPTARG"  ;;
+  f) pthStationsINI="$OPTARG" ;;
+  C) flRemoveCSV=1	      ;;
   y) (( $(fgrep processor /proc/cpuinfo | wc -l) > 1 )) && flParallelExec=1 ;;
   \?|*) doShowUsage; exit 1      ;;
  esac
@@ -70,30 +75,12 @@ shift $((OPTIND-1))
  exit 1
 }
 
-
-declare -A dblocks ul_point
-
- dblocks[default]='1-29.02.2012'
- 
-# dblocks[RostovOnDon]='11-17.05.2012 1-7.07.2012'
-#ul_point[RostovOnDon]='47.5 39.5'
-
-# dblocks[Remontnoe]='05.2012 06.2012 07.2012 08.2012'
-#ul_point[Remontnoe]='47.0 43.5'
-
-# dblocks[Kazanskaya]='5-11.08.2012'
-#ul_point[Kazanskaya]='50.0 41.0'
-
-# dblocks[Millerovo]='18-21.08.2012'
-#ul_point[Millerovo]='49.0 40.0'
-
-# dblocks[Taganrog]='05.2012 06.2012 07.2012 08.2012'
-#ul_point[Taganrog]='47.5 38.5'
-
-# dblocks[Konstantinovsk]='05.2012 06.2012 07.2012 08.2012'
-#ul_point[Konstantinovsk]='48.0 41.0'
- dblocks[Tsimlyansk]='03.2012'
-ul_point[Tsimlyansk]='48.0 42.0'
+if [[ -f $pthStationsINI && -r $pthStationsINI ]]; then
+ eval "$(read_ini $pthStationsINI)"
+else
+ echo "INI file $pthStationsINI is not accessible by me" >&2
+ exit 1
+fi
 
 baseDir="$TEMP_DIR/$DATA_ID"
 if [[ -d $baseDir ]]; then
@@ -102,8 +89,6 @@ else
  mkdir $baseDir
 fi
 
-unset wait4pids
-
 if [[ $flTestOutCmds ]]; then
  cmd='cat -'
 else
@@ -111,21 +96,23 @@ else
 fi
 
 {
- for meteoStationID in ${!ul_point[@]}; do
+ for meteoStationID in ${!INIdimensions[@]}; do
   mkdir -p $baseDir/$meteoStationID
-  for dblock in ${dblocks[$meteoStationID]-${dblocks[default]}}; do
+  POINT_FORECAST="${flTestOutCmds+$HOME/bin/point_forecast}${flResetSourcePath+ -s /store/GRIB/cooked/GFS4/${DATA_ID}}${mode+ -m $mode} %DEST% -n -H ${nPredictHours} '$(sqr_points ${INIdimensions[$meteoStationID]})'"
+  for dblock in ${INIdblocks[$meteoStationID]-${INIdblocks[default]}}; do
    mkdir -p $baseDir/$meteoStationID/$dblock
+   POINT_FORECAST="${POINT_FORECAST/\%DEST\%/-d $baseDir/$meteoStationID/$dblock}"
    for day in $(eval_dinv $dblock); do
-    cat <<EOF
-${flTestOutCmds+$HOME/bin/point_forecast}${flResetSourcePath+ -s /store/GRIB/cooked/GFS4/${DATA_ID}}${mode+ -m $mode} -n -d $baseDir/$meteoStationID/$dblock -H $nPredictHours '$(sqr_points ${ul_point[$meteoStationID]})' $day 0
-EOF
-   done   
+    echo "${POINT_FORECAST} $day 0"
+   done
   done
  done
 } | $cmd
 
 if [[ $emailTO && -d $DATA_ID ]]; then
  cd $TEMP_DIR
+ [[ $flRemoveCSV ]] && find $DATA_ID/ -type f -name '*.csv' -delete
  zip -r $DATA_ID{.zip,/}
  mailfile.pl -f ${DATA_ID}.zip -d $emailTO -T "Meteoreport inside (ID=${DATA_ID})"
+ rm -f ${DATA_ID}.zip
 fi
